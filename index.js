f06dbab80f60aa74aed95cf8d694b7c6061b8559
Lmaleque <lmale001@campus.goldsmiths.ac.uk>
2025-11-14 00:07:04 +0000

    33801543

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import express and ejs
 var express = require ('express')
+var mysql = require('mysql2')
 var ejs = require('ejs')
 const path = require('path')
 
@@ -18,6 +19,18 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // Define our application-specific data
 app.locals.shopData = {shopName: "Bertie's Books"}
+
+// Define the database connection pool
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'berties_books_app',
+    password: 'qwertyuiop',
+    database: 'berties_books',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+global.db = db;
 
 // Load the route handlers
 const mainRoutes = require("./routes/main")
